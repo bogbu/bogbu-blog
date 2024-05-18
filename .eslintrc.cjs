0bde67caf86a6384df103a69831db482c2380b77
jeongjihye <glutamine082442@gmail.com>
2024-05-18 17:01:20 +0900

    add eslint setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,42 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+ root: true,
+ extends: [
+  'eslint:recommended',
+  'plugin:@typescript-eslint/recommended',
+  'plugin:svelte/recommended',
+  'prettier',
+  'airbnb'
+ ],
+ parser: '@typescript-eslint/parser',
+ plugins: ['@typescript-eslint', 'react'],
+ parserOptions: {
+  sourceType: 'module',
+  ecmaVersion: 2020,
+  extraFileExtensions: ['.svelte']
+ },
+ env: {
+  browser: true,
+  es2017: true,
+  node: true
+ },
+ rules: {
+  'no-console': 'off',
+  'indent': ['error', 2],
+  'quotes': ['error', 'single'],
+  'semi': ['error', 'always'],
+ },
+ globals: {
+  Atomics: 'readonly',
+  SharedArrayBuffer: 'readonly',
+ },
+ overrides: [
+  {
+   files: ['*.svelte'],
+   parser: 'svelte-eslint-parser',
+   parserOptions: {
+    parser: '@typescript-eslint/parser'
+   }
+  }
+ ]
 };

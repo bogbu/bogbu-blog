5eacf72a1d4b4a2c3cd180a402da98ecb3eb9208
bogbu <glutamine082442@gmail.com>
2024-05-31 10:50:50 +0900

    setting env: setting public api key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+import { env } from '$env/dynamic/public';
+
 const supabase = createClient(
-    'https://lkgbxptqilkrzaodhglx.supabase.co', // Supabase URL
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrZ2J4cHRxaWxrcnphb2RoZ2x4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIwMjc4MjIsImV4cCI6MjAxNzYwMzgyMn0.QbNGVIm3RgfOeIxnzYwc8hOr1m1XxKTBecwNCmRfvPc' // Public API Key
+    env.PUBLIC_SUPABASE_URL,
+    env.PUBLIC_SUPABASE_ANON_KEY
 )
 
 export { supabase }

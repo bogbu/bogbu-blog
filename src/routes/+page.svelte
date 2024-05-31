11437c0d096bf6b2460840f0a9ab79ce343ace82
bogbu <glutamine082442@gmail.com>
2024-05-31 11:25:35 +0900

    add user example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import {supabase} from "$lib/supabase";
+	import type {User} from "@supabase/supabase-js";
 	const google = async () => {
 		await supabase.auth.signInWithOAuth({
 			provider: 'google',
@@ -10,6 +11,13 @@
 				},
 			},
 		});
+	}
+	let user:User;
+	const getUser = async () => {
+		const {data, error} = await supabase.auth.getUser();
+
+		if(data.user) user = data.user;
+		console.log(data,"error")
 	}
 </script>
 
@@ -25,7 +33,12 @@
 	<button on:click={google}>
 		google login
 	</button>
-
+	<button on:click={getUser}>
+		get user
+	</button>
+	{#if user}
+		<img src={user.user_metadata.avatar_url} alt="user.user_metadata.avatar_url"/>
+	{/if}
 </section>
 
 <style>

cc3155b4edd5b921a593f3eb0b29376b06ea521e
bogbu <glutamine082442@gmail.com>
2024-06-14 18:00:07 +0900

    add component, tailwind - draggable fun and component

--- src/lib/utils/draggable/dnd.ts
+++ src/lib/utils/draggable/dnd.ts
@@ -2,5 +2,8 @@ import type {DraggableType} from "$lib/types/Draggable";
 
 
 export const dragStart = ({currentTarget, dataTransfer}:DraggableType) => {
-    console.log(currentTarget, dataTransfer , 'event');
+    console.log(currentTarget, dataTransfer , 'dragStart event');
+}
+export const drop = ({currentTarget, dataTransfer}:DraggableType) => {
+    console.log(currentTarget, dataTransfer , 'drop event');
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 import {mockList} from "$lib/mock/mockList";
 import WindowFrame from "./_component/WindowFrame.svelte";
 import DragStart from "$lib/utils/draggable/DragStart.svelte";
+import {drop} from "$lib/utils/draggable/dnd";
 
 </script>
 
@@ -11,7 +12,7 @@ import DragStart from "$lib/utils/draggable/DragStart.svelte";
 </svelte:head>
 
 <section class="flex">
-	<article class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-[80%]">
+	<article class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-[80%]" on:drop={(e)=>{console.log(e,"e");drop({dataTransfer: e.dataTransfer, currentTarget : e.currentTarget})}}>
 		{#each mockList as item, i}
 			<div draggable="true" role="listitem" class="bg-white shadow-md p-4 min-h-[250px] rounded-lg">
 				{@html item}

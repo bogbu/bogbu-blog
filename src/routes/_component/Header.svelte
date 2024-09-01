601063f5645eccfc0c79d86af7d34e6ab38eb846
bogbu <glutamine082442@gmail.com>
2024-09-01 23:48:17 +0900

    edit main layout

--- src/routes/_component/Header.svelte
+++ src/routes/_component/Header.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 
 import UserProfile from "./UserProfile.svelte";
+import GNB from "./GNB.svelte";
 </script>
 
-<header>
+<header class="flex flex-col gap-3">
     <UserProfile/>
+    <GNB/>
 </header>
 
 <style lang="scss">
---
+++ src/routes/_component/TaskBar.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    // Add any necessary imports or logic here
+</script>
+
+<div class="taskbar">
+    <div class="start-button">Start</div>
+    <div class="task-items">
+        <button>test</button>
+        <!-- Task items will go here -->
+    </div>
+    <div class="system-tray">
+        <!-- System tray icons will go here -->
+    </div>
+</div>
+
+<style lang="scss">
+    .taskbar {
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #c0c0c0;
+        border-top: 2px solid #fff;
+        border-left: 2px solid #fff;
+        border-right: 2px solid #808080;
+        border-bottom: 2px solid #808080;
+        height: 40px;
+        padding: 0 5px;
+        font-family: 'dungGeunMo', serif;
+        .start-button {
+            background-color: #000080;
+            color: #fff;
+            padding: 5px 10px;
+            border: 2px solid #fff;
+            border-top: 2px solid #808080;
+            border-left: 2px solid #808080;
+            cursor: pointer;
+        }
+        .task-items {
+            flex-grow: 1;
+            display: flex;
+            align-items: center;
+            padding: 0 10px;
+        }
+
+        .system-tray {
+            display: flex;
+            align-items: center;
+        }
+    }
+
+</style>
